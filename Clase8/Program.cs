4fb97dcbdc5cb47afd2fe09bb46aae6e14fb7513
MazzGuille <mazzcode@gmail.com>
2022-05-23 00:49:40 -0300

    tarea8

--- Clase8/Program.cs
+++ Clase8/Program.cs
@@ -1,8 +1,10 @@
-﻿using static System.Console;
+﻿using System.Security.AccessControl;
+using static System.Console;
+using System.Collections;
 
 Clear();
 //1)Crear una matriz de dos dimensiones de tipo int llamada numeros.
-int [,] numeros;
+int[,] numeros;
 
 //2) Determinar el tamaño de cada dimansion (fila, columnas) mediante valores ingresados por pantalla.
 WriteLine("Indica el numero de filas que tendra la matriz");
@@ -24,7 +26,7 @@ double[] promedios;
 //4) Recorrer la matriz para su carga con elementos de tipo int
 for (int i = 0; i < numeros.GetLength(0); i++)
 {
-      
+
     for (int j = 0; j < numeros.GetLength(1); j++)
     {
         WriteLine($"Introduce un valor para la posicion {i} de la fila");
@@ -34,29 +36,88 @@ for (int i = 0; i < numeros.GetLength(0); i++)
         int valorCol = int.Parse(ReadLine());
 
 
-        numeros[i , j] = valorFila * valorCol ;
+        numeros[i, j] = valorFila * valorCol;
     }
 }
 
 //5) Recorrer la matriz para mostrar cada valor de la matriz
 for (int i = 0; i < numeros.GetLength(0); i++)
 {
-      
+
     for (int j = 0; j < numeros.GetLength(1); j++)
-    {   
+    {
         WriteLine();
-        WriteLine($"Cuando la posicion es: {i} , {j}; Valor = {numeros[i , j]}");
+        WriteLine($"Cuando la posicion es: {i} , {j}; Valor = {numeros[i, j]}");
         WriteLine();
     }
 }
 
 
+int[,] arrayTest;
+
+arrayTest = new int[,]
+{
+{1}, {2},
+{3}, {4}
+};
+
+double[] vectorTest;
+
+
+
+for(int i = 0; i < arrayTest.GetLength(0); i++)
+{
+    int suma = 0;
+    for(int j = 0; j < arrayTest.GetLength(1); j++)
+    {
+        suma += arrayTest[i,j];
+    }
+    WriteLine($"Linea {i} = {suma}");
+}
+
+for(int j = 0; j < arrayTest.GetLength(0); j++)
+{
+    int suma = 0;
+    for(int i = 0; i < arrayTest.GetLength(1); i++)
+    {
+        suma += arrayTest[i,j];
+    }
+     WriteLine($"columna {j} = {suma}");    
+}
+
+
+
+
+
+
+
+double columna11 = numeros[0, 0]; // esto hay que ponerlo de alguna manera dinamico
+double columna21 = numeros[1, 0];
+double columna12 = numeros[0, 1];
+double columna22 = numeros[1, 1];
+WriteLine($"Valor columna11: {columna11}");
+WriteLine($"Valor columna21: {columna21}");
+WriteLine($"Valor columna12: {columna12}");
+WriteLine($"Valor columna22: {columna22}");
+WriteLine();
+
+
+double vector0 = (columna11 + columna21) / 2;
+double vector1 = (columna12 + columna22) / 2;
+WriteLine($"Vector 0: {vector0}");
+WriteLine($"Vector 1: {vector1}");
+WriteLine();
+
+
+
+promedios = new double[] { vector0, vector1 };
+WriteLine(promedios[0] + promedios[1]);
+
+
+
+
 //6) Calcular el promedio de cada columna y asignarlo a la posicion correspondiente dentro del vector promedios
-promedios = new double[numeros.GetLength(1)];
-WriteLine();
-WriteLine($"El tamaño del vector es: {promedios.Length}");
-WriteLine();
-WriteLine($"El promedio del vector es: {promedios.Average()}");
+
 
 
 //7) Mostrar los promedios recorriendo el vector promedios
